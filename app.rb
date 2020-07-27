f0640d72fd55726e5180dd386f434af7e04c9909
Patrick A <akolo@hotmail.com>
2020-07-27 13:41:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My Name is Patrick"
+    "My Name is "
   end
 
   get '/hometown' do
-  "My hometown is Germantown"
+  "My hometown is "
   end
 
   get '/favorite-song' do
-  "My favorite song is Happy by Farrell Williams"
+  "My favorite song is "
   end
 
 end
